7487175246e1b595293bfd19cbe5312867d0a3d3
Ibrahim Cisse <ibrahimcisse@Ibrahims-MacBook-Air.local>
2019-09-23 20:25:47 -0400

    Updates

---
+++ precache-manifest.a6b2398a40a38d972172749a153a1b30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8cee14c1a5a56df7d123171f81fc48",
+    "url": "/profilecard/index.html"
+  },
+  {
+    "revision": "0dffd0e1aff35df4b026",
+    "url": "/profilecard/static/css/main.32f4c368.chunk.css"
+  },
+  {
+    "revision": "c70d5f67c7d6da9485d0",
+    "url": "/profilecard/static/js/2.734bc41b.chunk.js"
+  },
+  {
+    "revision": "0dffd0e1aff35df4b026",
+    "url": "/profilecard/static/js/main.212fe600.chunk.js"
+  },
+  {
+    "revision": "08c273a0fe208e11ee0d",
+    "url": "/profilecard/static/js/runtime-main.91c7ba46.js"
+  }
+]);
